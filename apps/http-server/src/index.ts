cc00331ed41e3ea4bf3f364b89ffb409a07c5c46
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2025-02-11 23:16:43 +0530

    first commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,23 @@
+import  express from "express";
+import {client} from "@repo/db/client";
+const app=express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("hi there");
+})
+
+app.post("/signup",async (req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+  const user=await client.user.create({
+        data:{
+            username:username,
+            password:password
+        }
+    })
+    res.json({
+        messagw:"signed up successfull"
+    })
+})
+app.listen(3002);
